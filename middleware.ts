bce0ba5a9e80cc75c6c387267ba9453d67b5aeb5
DvlPrSujalPatel <sujal7455@gmail.com>
2024-09-14 12:34:03 +0530

    error fixes

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
-// Middleware to handle authentication
 export default authMiddleware({
-  // Define which routes are public and don't require authentication
   publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
 });
 
-// Configuration for which routes should use this middleware
 export const config = {
-  // Define the path patterns to include for this middleware
-  matcher: [
-    // Match all routes except those with file extensions or under the _next directory
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Specifically include the root path
-    "/",
-    // Match API routes and TRPC routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
